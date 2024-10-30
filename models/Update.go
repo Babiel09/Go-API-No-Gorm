c7ecb8588c54142b83778cdcdd10711ab40b9268
Gabriel Castro <estudosbabiel@gmail.com>
2024-10-30 20:10:47 -0300

    feat: Update informations

---
+++ models/Update.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/Babiel09/database"
+
+func Update(id uint, peoples People) (int64, error) {
+	coon, err := database.DatabaseConnection()
+	if err != nil {
+		return 0, err
+	}
+	defer coon.Close()
+
+	res, err := coon.Exec(`UPDATE people SET created-at=$2, nome=$3, sexualidade=$4 WHERE id=$1`, id, peoples.Createdat, peoples.Nome, peoples.Sexualidade)
+	if err != nil {
+		return 0, err
+	}
+	return res.RowsAffected()
+
+}
